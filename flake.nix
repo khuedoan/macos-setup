0aedc79d6e16c76b5324c0fd9e85506f342cdcad
Khue Doan <mail@khuedoan.com>
2024-03-16 00:21:05 +0700

    chore: update packages

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      url = "github:nixos/nixpkgs/nixos-23.11";
     };
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
