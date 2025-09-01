d6056e6e5f2a11a170d4cfdf8486f1300572e054
Khue Doan <kdoan@axon.com>
2025-09-01 13:39:03 +0700

    refactor!: remove AeroSpace
    Still not fast enough on macOS.

--- configuration.nix
+++ configuration.nix
@@ -140,63 +140,6 @@
   services = {
     karabiner-elements.enable = true;
     tailscale.enable = true;
-    aerospace = {
-      enable = true;
-      settings = {
-        mode.main.binding = {
-          alt-h = "focus left";
-          alt-j = "focus down";
-          alt-k = "focus up";
-          alt-l = "focus right";
-
-          alt-shift-h = "move left";
-          alt-shift-j = "move down";
-          alt-shift-k = "move up";
-          alt-shift-l = "move right";
-
-          alt-ctrl-h = "join-with left";
-          alt-ctrl-j = "join-with down";
-          alt-ctrl-k = "join-with up";
-          alt-ctrl-l = "join-with right";
-
-          alt-1 = "workspace 1";
-          alt-2 = "workspace 2";
-          alt-3 = "workspace 3";
-          alt-4 = "workspace 4";
-          alt-5 = "workspace 5";
-          alt-6 = "workspace 6";
-          alt-7 = "workspace 7";
-          alt-8 = "workspace 8";
-          alt-9 = "workspace 9";
-          alt-0 = "workspace 10";
-
-          alt-shift-1 = "move-node-to-workspace 1";
-          alt-shift-2 = "move-node-to-workspace 2";
-          alt-shift-3 = "move-node-to-workspace 3";
-          alt-shift-4 = "move-node-to-workspace 4";
-          alt-shift-5 = "move-node-to-workspace 5";
-          alt-shift-6 = "move-node-to-workspace 6";
-          alt-shift-7 = "move-node-to-workspace 7";
-          alt-shift-8 = "move-node-to-workspace 8";
-          alt-shift-9 = "move-node-to-workspace 9";
-          alt-shift-0 = "move-node-to-workspace 10";
-
-          alt-tab = "workspace-back-and-forth";
-
-          alt-f = "fullscreen --no-outer-gaps";
-          alt-shift-f = "layout floating tiling";
-          alt-t = "layout tiles h_accordion";
-
-          alt-ctrl-minus = "resize smart -50";
-          alt-ctrl-equal = "resize smart +50";
-        };
-
-        automatically-unhide-macos-hidden-apps = true;
-        # TODO currently disable padding so Ghostty doesn't jump around
-        # https://ghostty.org/docs/help/macos-tiling-wms
-        accordion-padding = 0;
-      };
-    };
   };
 
   nix = {
